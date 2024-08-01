7b287603ca16a25a5e5eee6c26b6ae427cd227e7
marhaendev <108604908+marhaendev@users.noreply.github.com>
2024-08-02 02:43:23 +0700

    Update virtualmin.sh

--- virtualmin.sh
+++ virtualmin.sh
@@ -8,19 +8,15 @@ echo "Memperbarui dan meng-upgrade sistem..."
 apt update -y && apt upgrade -y
 
 echo "Menginstal dependensi dasar..."
-apt install -y wget curl gnupg
+apt install -y wget curl gnupg python3-venv
 
 echo "Menambahkan repositori Virtualmin..."
-wget -O- https://software.virtualmin.com/gpl/scripts/install.sh | sudo bash
-
-echo "Memulai instalasi Virtualmin..."
-# Skrip instalasi Virtualmin akan dijalankan di sini
-# Anda akan diminta untuk mengikuti beberapa langkah konfigurasi
+wget -O - https://software.virtualmin.com/gpl/scripts/install.sh | bash
 
 echo "Instalasi Virtualmin selesai. Mohon ikuti petunjuk di layar untuk menyelesaikan konfigurasi."
 
 echo "Periksa status Virtualmin dengan menggunakan perintah berikut setelah instalasi selesai:"
-echo "sudo systemctl status webmin"
+echo "systemctl status webmin"
 
 echo "Akses Virtualmin di: https://<IP_SERVER_AKSES>:10000"
 
