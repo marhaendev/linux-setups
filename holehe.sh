6f26b4c2f9371ee3f6239c8a42e6e93b884e2be5
marhaendev <108604908+marhaendev@users.noreply.github.com>
2024-08-09 18:12:52 +0700

    Update holehe.sh

--- holehe.sh
+++ holehe.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Menyelesaikan masalah dpkg
-echo "Menyelesaikan masalah dpkg..."
-dpkg --configure -a
+# Memperbaiki masalah dpkg
+echo "Memperbaiki masalah dpkg..."
+sudo dpkg --configure -a
 
 # Memperbarui sistem dan menginstal git
 echo "Memperbarui sistem dan menginstal git..."
-apt update && apt upgrade -y
-apt install -y python3 python3-pip git
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3 python3-pip python3-venv git
 
 # Meng-clone repository Holehe
 echo "Meng-clone repository Holehe..."
 rm -rf holehe  # Menghapus direktori lama jika ada
 git clone https://github.com/megadose/holehe.git
 
-# Menginstal Holehe secara global menggunakan pipx
-echo "Menginstal Holehe menggunakan pipx..."
-pipx install holehe
+# Membuat dan mengaktifkan virtual environment
+echo "Membuat dan mengaktifkan virtual environment..."
+cd holehe || { echo "Direktori holehe tidak ditemukan!"; exit 1; }
+python3 -m venv holehe-env
+source holehe-env/bin/activate
+
+# Menginstal Holehe
+echo "Menginstal Holehe..."
+pip install .
 
 # Menambahkan alias untuk Holehe (Opsional)
 echo "Menambahkan alias untuk Holehe..."
-echo "alias holehe='$(which holehe)'" >> ~/.bashrc
+echo "alias holehe='~/holehe/holehe-env/bin/holehe'" >> ~/.bashrc
 source ~/.bashrc
 
 # Menyelesaikan
